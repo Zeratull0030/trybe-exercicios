01f65aa78bebd4c2692d31ab93f55edd9cd0a3a7
Caio Ribo <caio.hrmportugal@gmail.com>
2023-03-28 19:23:19 -0300

    Exercício dos dias da semana

---
+++ Fundamentos/3. Introdução a JavaScript/Exercicio2-3.1-OperadoreslógicosOR.js
@@ -0,0 +1,14 @@
+//Crie uma variável chamada “weekDay” que receba a string “quarta-feira”. - OK
+//Utilizando if/else, implemente as seguintes condicionais:
+//Se a variável “weekDay” for “segunda-feira”, “terça-feira”, “quarta-feira”, “quinta-feira” ou “sexta-feira”, imprima “Oba, mais um dia de aprendizado na Trybe >:D”.
+//Se a variável for algum dia de fim de semana, imprima “FINALMENTE, descanso merecido! UwU!”.
+//Experimente trocar o valor da string ou até mesmo montar seu próprio algoritmo. Esse assunto é muito importante para seu aprendizado!
+
+let weekDay = "Domingo"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira")
+{console.log ("Oba, mais um dia de aprendizado na Trybe >:D")}
+
+else 
+{console.log ("FINALMENTE, descanso merecido! UwU!");
+}
