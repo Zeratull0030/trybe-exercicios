e6d73b0b18af67e09220a4e87a2c3380925a0473
Caio Ribo <caio.hrmportugal@gmail.com>
2023-03-29 20:15:16 -0300

    Passo 05 do exercício - Imprimindo os valores contidos no array

--- Fundamentos/3. Introdução a JavaScript/dia-2-JavaScript-Aray-Loop-For/exercicio1-lidandocomarray.js
+++ Fundamentos/3. Introdução a JavaScript/dia-2-JavaScript-Aray-Loop-For/exercicio1-lidandocomarray.js
@@ -54,9 +54,22 @@ console.log(maiorvalor)
 
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    result += 1;
+  }
+}
+
+if (result === 0) {
+  console.log('nenhum valor ímpar encontrado');
+} else {
+  console.log(result);
+}
 
 
-
+console.log("essa eu não consegui fazer")
 
 //Utilizando for, descubra o menor valor contido no array e imprima-o.
 
@@ -80,4 +93,7 @@ teste.push(index);
 
 console.log(teste)
 //Utilizando o array que acabou de criar, imprima o resultado da divisão de cada um dos elementos por 2.
+
+for(let index =0; index< numbers.length; index += 1){
+    console.log(teste[index]/2)};
 
