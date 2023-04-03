f3490a693f384a677cc9516c3b3dda54229785a0
Caio Ribo <caio.hrmportugal@gmail.com>
2023-04-03 20:31:11 -0300

    Primeiro exercício de fixação- Objetos

---
+++ Fundamentos/3. Introdução a JavaScript/Objetos/Objetos-ExerciciosDeFixação.js
@@ -0,0 +1,13 @@
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+let Player= {
+name:'Marta',
+lastName: 'Silva',
+age: 34,
+medals:{golden:2, silver:3}
+}
+
+console.log("A jogadora" +" ", Player.name + " ",Player.lastName + " ","tem " + Player.age + " anos de idade")
