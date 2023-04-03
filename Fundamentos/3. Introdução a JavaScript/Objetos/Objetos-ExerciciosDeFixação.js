d7029f9ffd36a9e459314aca59754b2d510bdba5
Caio Ribo <caio.hrmportugal@gmail.com>
2023-04-03 20:46:17 -0300

    Segundo exercício de fixação

--- Fundamentos/3. Introdução a JavaScript/Objetos/Objetos-ExerciciosDeFixação.js
+++ Fundamentos/3. Introdução a JavaScript/Objetos/Objetos-ExerciciosDeFixação.js
@@ -2,12 +2,21 @@ let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
 
 let Player= {
 name:'Marta',
 lastName: 'Silva',
 age: 34,
-medals:{golden:2, silver:3}
+medals:{golden:2, silver:3},
+bestInTheWorld:[2006, 2007, 2008, 2009, 2010, 2018]
+
 }
 
-console.log("A jogadora" +" ", Player.name + " ",Player.lastName + " ","tem " + Player.age + " anos de idade")
+//console.log("A jogadora" +" ", Player.name + " ",Player.lastName + " ","tem " + Player.age + " anos de idade")
+
+
+
+//Segundo Exercício ----------------------------------------
+
+console.log("A jogadora" +" ", Player['name'] + " ",Player['lastName'] + " ", "Foi eleita a melhor do mundo por",+ Player.bestInTheWorld.length + " vezes.")
